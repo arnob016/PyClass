6e587c3d4fc8053022bd06ae1aa29c82f8fdd7d5
TrainerADP016 <arnob15-375@diu.edu.bd>
2022-09-14 14:35:11 +0600

    update

---
+++ beecrowd.py
@@ -0,0 +1,7 @@
+a= float(input())
+b= float(input())
+c= float(input())
+ar= (a*2+b*3+c*5)/(2+3+5)
+ar='{0:.1f}'.format(ar)
+print('MEDIA =',end=' ')
+print(ar)
---
+++ watermelon.py
@@ -0,0 +1,4 @@
+i = int(input())
+if i > 2 and i%2==0:
+    print('YES')
+else: print('NO')
