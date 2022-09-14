17444deda5d099ef9d7428ff8bb15b38ff289124
TrainerADP016 <arnob15-375@diu.edu.bd>
2022-09-14 14:33:32 +0600

    Update aftert a long time

---
+++ try.py
@@ -0,0 +1,14 @@
+for p in range(0,10):
+    print('p = ',p,end=' ,')
+print()
+print()
+for x in range(0,10+1):
+    print('x = ',x,end=' ,')
+print() 
+print()   
+for i in range(10,0,-2):
+    print('i = ',i, end=' ,')
+print()  
+print()  
+for j in range(0,10,2):
+    print('j = ',j,end=' ,')
