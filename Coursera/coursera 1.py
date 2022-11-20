3aa55e2039f3ed07f8a4bb26bc58b740eba3ef00
TrainerADP016 <arnob15-375@diu.edu.bd>
2022-11-21 01:15:51 +0600

    Coursera course

---
+++ Coursera/coursera 1.py
@@ -0,0 +1,11 @@
+#Question 2
+#Fill in the blanks to make the factorial function return the factorial of n. Then, print the first 10 factorials (from 0 to 9) with the corresponding number. Remember that the factorial of a number is defined as the product of an integer and all integers before it. For example, the factorial of five (5!) is equal to 1*2*3*4*5=120. Also recall that the factorial of zero (0!) is equal to 1.
+
+def factorial(n):
+    result = 1
+    for x in range(1,n+1):
+        result *= x
+    return result
+
+for n in range(10):
+    print(n, factorial(n))
